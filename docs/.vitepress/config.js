5f7e0473fe7859e29123df50cbaef149c846bae9
neoNee <ruixuan_ni@yeah.net>
2025-10-22 10:11:43 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,52 @@
+// docs/.vitepress/config.js
+export default {
+  base: '/blog/',
+  // 站点级选项
+  title: "前端逆光者", // 网站标题
+  description: "前端逆光者用来写博客的地方", // 网站描述
+
+  themeConfig: {
+    // 主题级选项
+    nav: [
+      { text: '关于', link: '/about' },
+      {
+        text: '大前端',
+        items: [
+          { text: "html", link: "/bigFrontEnd/html/" },
+          { text: "css", link: "/bigFrontEnd/css/" },
+          { text: "js", link: "/bigFrontEnd/js/" },
+        ]
+      }
+    ],
+    sidebar: {
+      "/bigFrontEnd/html/": {
+        text: "html",
+        items: [
+          { text: "html", link: "/bigFrontEnd/html/" },
+          { text: "html1", link: "/bigFrontEnd/html/html1" },
+          { text: "html2", link: "bigFrontEnd/html/html2" },
+        ],
+      },
+      "/bigFrontEnd/css/": {
+        text: "css",
+        items: [
+          { text: "css", link: "/bigFrontEnd/css/" },
+          { text: "css1", link: "/bigFrontEnd/css/css1" },
+          { text: "css2", link: "/bigFrontEnd/css/css2" },
+        ],
+      },
+      "/bigFrontEnd/js/": {
+        text: "js",
+        items: [
+          { text: "js", link: "/bigFrontEnd/js/" },
+          { text: "js1", link: "/bigFrontEnd/js/js1" },
+          { text: "js2", link: "/bigFrontEnd/js/js2" },
+        ],
+      },
+    },
+    docFooter: {
+      prev: "上一篇",
+      next: "下一篇",
+    },
+  },
+};
---
+++ docs/bigFrontEnd/css/index.md
@@ -0,0 +1,6 @@
+# html 专题
+
+## 目录
+
+- [css1](./css1.md)
+- [css2](./css2.md)
